6b285ebb816cc3690ae5466c518a7230f786c93d
meoki-wong <1084415961@qq.com>
2023-03-01 10:25:04 +0800

    feat: 引入axios

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import 'lib-flexible/flexible'
 import Vant from 'vant';
 import 'vant/lib/index.css';
-
+import './api/axios.module'
 Vue.use(Vant);
 Vue.config.productionTip = false
 
