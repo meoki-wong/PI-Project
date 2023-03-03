a99e64d6e8c470546b0e4125f94ac107f625756d
meoki-wong <1084415961@qq.com>
2023-03-03 15:43:07 +0800

    --

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -109,14 +108,6 @@ const routes = [
     name: 'FAQ',
     title: "常见问题",
     component: () => import('../views/home/home-cjwt.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
