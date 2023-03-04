3dd0c16a6b62752057b5ac5b8b254f4e34c1cc53
meoki-wong <1084415961@qq.com>
2023-03-04 10:12:53 +0800

    --

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,25 @@
       <van-tabbar-item @click="$router.push('/')">
         <span>首页</span>
         <template #icon="props">
-          <img class="common-img" src="./assets/home-icon.png" />
+          <img class="common-img" :src="props.active ? require('./assets/home-icon.png') : require('./assets/home.png')" />
         </template>
       </van-tabbar-item>
       <van-tabbar-item @click="$router.push('/myloan')">
         <span>账单</span>
         <template #icon="props">
-          <img class="common-img" src="./assets/zd-icon.png" />
+          <img class="common-img" :src="!props.active ? require('./assets/zd-icon.png'): require('./assets/loan-check.png')" />
         </template>
       </van-tabbar-item>
       <van-tabbar-item @click="$router.push('/kf')">
         <span>客服</span>
         <template #icon="props">
-          <img class="common-img" src="./assets/kf-icon.png" />
+          <img class="common-img" :src="!props.active ? require('./assets/kf-icon.png'): require('./assets/kf-check.png')" />
         </template>
       </van-tabbar-item>
       <van-tabbar-item @click="$router.push('/my')">
         <span>我的</span>
         <template #icon="props">
-          <img class="common-img" src="./assets/my-icon.png" />
+          <img class="common-img" :src="!props.active ? require('./assets/my-icon.png'): require('./assets/my-check.png')" />
         </template>
       </van-tabbar-item>
     </van-tabbar>
